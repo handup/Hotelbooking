f7032a08319e93f0092cb23a34c79364985b99c6
Pavel-Groundley <andrei@groundley.com>
2023-11-25 15:22:24 +0100

    Added passportjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { HotelModule } from './hotel/hotel.module';
 import { RoomModule } from './room/room.module';
 import { BookingModule } from './booking/booking.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/test'), HotelModule, RoomModule, BookingModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/test'), HotelModule, RoomModule, BookingModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
