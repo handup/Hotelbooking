b54fc100c5889a916b2ec6e2be13967c30586af5
Pavel-Groundley <andrei@groundley.com>
2023-12-13 17:15:38 +0100

    updated DTO

--- src/room/dto/create-room.dto.ts
+++ src/room/dto/create-room.dto.ts
@@ -2,15 +2,23 @@ import { ApiProperty } from "@nestjs/swagger";
 import { Hotel } from "src/hotel/entities/hotel.entity";
 
 export class CreateRoomDto {
-    @ApiProperty({default: 1, required: false})
+    @ApiProperty({ default: 1, required: false })
     readonly adultGuests: number;
-    @ApiProperty({default: 1, required: false})
+    @ApiProperty({ default: 1, required: false })
     readonly childGuests: number;
-    @ApiProperty({default: 1, required: false})
+    @ApiProperty({ default: 1, required: false })
     readonly infantGuests: number;
-    @ApiProperty({default: "Steve", required: false})
+
+    @ApiProperty({ default: 1, required: false })
+    readonly price: number;
+
+    @ApiProperty({ default: "description", required: false })
+    readonly description: "description";
+    @ApiProperty({ default: "image", required: false })
+    readonly image: "image";
+    @ApiProperty({ default: "Steve", required: false })
     readonly label: string;
-    @ApiProperty({default: "Steve", required: false})
+    @ApiProperty({ default: "Steve", required: false })
     readonly hotelId: string;
     hotel: Hotel;
 }
